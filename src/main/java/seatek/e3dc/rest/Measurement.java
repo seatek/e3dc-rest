9e7df5eadabe20d44b3b6cb2980e72fe54cbb933
Stefan Krauthausen <s.krauthausen@ff-muenchen.de>
2018-08-25 22:00:33 +0200

    Support for Battery power and capacity.
    Cleanups.

--- src/main/java/seatek/e3dc/rest/Measurement.java
+++ src/main/java/seatek/e3dc/rest/Measurement.java
@@ -1,12 +1,18 @@
 package seatek.e3dc.rest;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Measurement {
 	private int solarleistung;
 	private int netzleistung;
 	private int hausleistung;
+	private int batterieleistung;
+	private int batteriekapazität;
 }
--- src/main/java/seatek/e3dc/rest/RestAccess.java
+++ src/main/java/seatek/e3dc/rest/RestAccess.java
@@ -1,45 +1,17 @@
 package seatek.e3dc.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestAccess {
-	/**
-	 * Injected property. May come from Command Line Arguments,
-	 * System.getProperties(), application.properties, ...
-	 */
-	@Value("${greeting}")
-	private String greeting;
 
 	@Autowired
 	private ModbusRepository modbusRepository;
 
-
-	@RequestMapping("/greeting")
-	public String greet() {
-		return this.greeting;
-	}
-
-	@RequestMapping("/reading/hausleistung")
-	public int getHausleistung() {
-		return modbusRepository.getHausleistung();
-	}
-
-	@RequestMapping("/reading/solarleistung")
-	public int getSolarleistung() {
-		return modbusRepository.getSolarleistung();
-	}
-
-	@RequestMapping("/reading/netzleistung")
-	public int getNetzleistung() {
-		return modbusRepository.getNetzleistung();
-	}
-
 	@RequestMapping("/")
 	public Measurement getMeasurement() {
-		return modbusRepository.getMeasurement();
+		return this.modbusRepository.getMeasurement();
 	}
 }
